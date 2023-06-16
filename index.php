cb0f824a3f7a12b375909597e2d3617cee837080
minninn <95167298+minninn@users.noreply.github.com>
2023-06-16 11:22:35 +0900

    login page

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     echo "<form name='login' method='post'>";
     echo "<input type='text' name='id' placeholder='ID'>";
     echo "<input type='password' name='password' placeholder='PASSWORD'>";
-    echo "<input type='submit' name='submit' value='login'>";
+    echo "<input type='submit' name='submit' value='login'></form>";
 
     if( isset( $_POST['submit'] ) ) {
         $id = $_POST['id'];
@@ -24,6 +24,7 @@
             header( "location:xss.php" );
         } else {
             echo "<script>alert('아이디 또는 비밀번호가 잘못되었습니다.');</script>";
+            echo "<!-- SELECT test_id FROM test_table WHERE test_id = 'ID' AND test_pw = 'PASSWORD'; -->";
         };
     };
 ?>
