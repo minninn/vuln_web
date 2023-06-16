3d835c88e9ac3b5b9e87519677eb5ed390dfa247
minninn <95167298+minninn@users.noreply.github.com>
2023-06-16 10:30:27 +0900

    loginpage

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    $conn = mysqli_connect(
+                'localhost',
+                'xss',
+                'xss_password',
+                'test_login'
+    );
+
+    echo "<form name='login' method='post'>";
+    echo "<input type='text' name='id' placeholder='ID'>";
+    echo "<input type='password' name='password' placeholder='PASSWORD'>";
+    echo "<input type='submit' name='submit' value='login'>";
+
+    if( isset( $_POST['submit'] ) ) {
+        $id = $_POST['id'];
+        $pw = $_POST['password'];
+
+        $sqlq = "SELECT test_id FROM test_table WHERE test_id = '$id' AND test_pw = '$pw'";
+        $result = $conn -> query( $sqlq );
+
+        if( $result -> num_rows > 0 ) {
+            session_start();
+            $_SESSION["id"] = $id;
+            header( "location:xss.php" );
+        } else {
+            echo "<script>alert('아이디 또는 비밀번호가 잘못되었습니다.');</script>";
+        };
+    };
+?>
