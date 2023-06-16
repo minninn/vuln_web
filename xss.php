7c0ed5cbabdb6e6f05d8a3893232b383a097b857
minninn <95167298+minninn@users.noreply.github.com>
2023-06-16 11:22:12 +0900

    xss vulnerable page

--- xss.php
+++ xss.php
@@ -10,7 +10,11 @@
 
     if( isset( $_SESSION["id"] ) ) {
 
-        if( $_SESSION["id"] !== '[ID]' ) {
+        if( $_SESSION["id"] === 'admin' ) {
+            echo "<script>location.replace('591797f6e3fe230183ad5be1783900c71d5036c7b72d6326a207e1f324a31b603.html');</script>";
+        };
+
+        if( $_SESSION["id"] !== 'piolink' ) {
             echo "<script>";
             echo "setTimeout( function() { alert('세션이 유효하지 않습니다.'); }, 0 );";
             echo "</script>";
